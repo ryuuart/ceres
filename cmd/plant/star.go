85a33aba85456e9cf5c67186acc954500a412ac1
Long Nguyen <me@long-nguyen.dev>
2024-10-11 17:53:05 -0500

    feat: add some argument validation

--- cmd/plant/star.go
+++ cmd/plant/star.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/charmbracelet/huh"
 	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
@@ -18,34 +17,33 @@ var StarCmd = &cobra.Command{
 	Short: "Creates a new star system in REI",
 	Long:  `Creates a new directory that's a star type. A star type directory is a project in REI.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		var name string
+		if len(args) == 0 || len(args[0]) == 0 {
+			log.Fatal("You have to provide the star's name")
+		} else {
+			isWrongFormat, _ := regexp.MatchString(`(^[-\s]+.*)|(.*[-\s]+$)|(-(-)+)`, args[0])
+
+			if isWrongFormat {
+				log.Fatal(`The format is wrong. Your name can't be blank, start, or end with one or more "-" or spaces. Don't include consecutive "-" too.`)
+			}
+		}
+		name := args[0]
 		uuid := strings.ToLower(uuid.NewString())
 		date := time.Now().Format("01-02-2006")
 
-		// get the name
-		form := huh.NewForm(
-			huh.NewGroup(
-				huh.NewInput().Title("What's the name of the star?").Value(&name),
-			),
-		)
+		// Format the name
+		re, _ := regexp.Compile(`[^\w\s-]`)
+		name = re.ReplaceAllString(name, "")
 
-		err := form.Run()
+		re, _ = regexp.Compile(`\s+`)
+		name = re.ReplaceAllString(name, "-")
+		name = strings.ToLower(name)
+
+		formatted_name := fmt.Sprintf("%s--star--%s--%s", name, date, uuid)
+
+		// Create the folder
+		err := os.Mkdir(formatted_name, os.ModePerm)
 		if err != nil {
 			log.Fatal(err)
-		} else {
-			// Format the name
-			re, _ := regexp.Compile(`[^\w\s-]`)
-			name = re.ReplaceAllString(name, "")
-
-			re, _ = regexp.Compile(`\s+`)
-			name = re.ReplaceAllString(name, "-")
-			name = strings.ToLower(name)
-
-			formatted_name := fmt.Sprintf("%s--star--%s--%s", name, date, uuid)
-			err = os.Mkdir(formatted_name, os.ModePerm)
-			if err != nil {
-				log.Fatal(err)
-			}
 		}
 	},
 }
